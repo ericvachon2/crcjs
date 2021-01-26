0db7767974e305d4e1611f02e99dc3073377bbc2
ericvachon2 <ericguay@hotmail.ca>
2021-01-26 12:32:46 -0500

    commit

---
+++ Lab 1/2ndFunctionsAndAlert/script.js
@@ -0,0 +1,21 @@
+
+		function sum(a,b){
+			let c = 0;
+			c = a + b;
+			alert("The sum is: " + c);
+		}
+		function sub(a,b){
+			let c = 0;
+			c = a - b;
+			alert("the difference is: " + c);
+		}
+		function mul(a,b){
+			let c = 0;
+			c = (a * b);
+			alert("the total is: " + c);
+		}
+		function div(a,b){
+			let c = 0;
+			c = (a / b);
+			alert("the final value is: " + c)
+		}
